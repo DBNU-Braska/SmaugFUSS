28e3d770365023c877ea625479a96c1c6ad61a76
braskaalucard <64438452+braskaalucard@users.noreply.github.com>
2020-06-30 09:00:09 -0400

    Update fight.c
    added do_stopspar and do_spar

--- src/fight.c
+++ src/fight.c
@@ -5877,6 +5877,224 @@ void dam_message( CHAR_DATA * ch, CHAR_DATA * victim, int dam, int dt )
 }
 #endif
 
+void do_stopspar( CHAR_DATA * ch, char * argument )
+{
+    CHAR_DATA * victim;
+
+	if( ( victim = who_fighting( ch ) ) == NULL )
+    {
+        send_to_char( "You arn't fighting anyone.\n\r", ch );
+        return;
+    }
+	if( IS_NPC( victim ) )
+    {
+        send_to_char( "You think you are sparing?!?.\n\r", ch );
+    	return;
+    }
+
+	if ( !IS_NPC( ch ) && !IS_NPC( victim ) && !xIS_SET( ch->act, PLR_SPAR ) && !xIS_SET( victim->act, PLR_SPAR ) )
+    {
+        send_to_char( "You think you are sparing?!?.\n\r", ch );
+        return;
+    }
+	stop_fighting( ch, TRUE );
+	act( AT_GREEN, "You decide to stop sparring with $N.", ch, NULL, victim, TO_CHAR );
+	act( AT_GREEN, "$n decides to stop sparring with you.", ch, NULL, victim, TO_VICT );
+	act( AT_GREEN, "$n decides to stop sparring with $N.", ch, NULL, victim, TO_NOTVICT );
+	ch->pcdata->sparcount += 1;
+	victim->pcdata->sparcount += 1;
+
+    if( ch->pcdata->nextspartime <= 0 )
+    {
+        struct tm *tms;
+        tms = localtime( &current_time );
+        tms->tm_mday += 1;
+        ch->pcdata->nextspartime = mktime( tms );
+    }
+    if( victim->pcdata->nextspartime <= 0 )
+    {
+        struct tm *tms;
+        tms = localtime( &current_time );
+        tms->tm_mday += 1;
+        victim->pcdata->nextspartime = mktime( tms );
+    }
+
+    long double cspar = 0;
+    long double vspar = 0;
+    cspar = ch->exp - ch->spar_start;
+    vspar = victim->exp - victim->spar_start;
+    ch_printf(ch,"&cTotal pl gained this spar: &C%s\n\r", num_punct_ld(cspar) );
+    ch_printf(victim,"&cTotal pl gained this spar: &C%s\n\r", num_punct_ld(vspar) );
+    ch->spar_start = 0;
+    victim->spar_start = 0;
+    return;
+}
+
+void do_spar( CHAR_DATA * ch, char * argument )
+{
+    char buf[MAX_STRING_LENGTH];
+    char arg[MAX_INPUT_LENGTH];
+    CHAR_DATA *victim;
+
+    one_argument( argument, arg );
+
+    if( arg[0] == '\0' )
+    {
+        send_to_char( "Spar whom?\n\r", ch );
+        return;
+    }
+    if( ( victim = get_char_room( ch, arg ) ) == NULL )
+    {
+        send_to_char( "They aren't here.\n\r", ch );
+        return;
+    }
+    if( !IS_NPC( victim ) && !victim->desc )
+    {
+        send_to_char( "They are link dead, it wouldn't be right.", ch );
+        return;
+    }
+	if( IS_NPC( victim ) )
+    {
+        send_to_char( "You can only spar other players.", ch );
+        return;
+    }
+    if( victim == ch )
+    {
+        send_to_char( "You can't spar your self.\n\r", ch );
+        return;
+    }
+	if( !IS_NPC( ch ) && !IS_NPC( victim ) && ch -> pcdata -> sparcount >= 100 )
+	{
+		send_to_char( "You are too tired from sparring to spar further today.\n\r",ch );
+		return;
+	}
+	if( !IS_NPC( ch ) && !IS_NPC( victim ) && victim -> pcdata -> sparcount >= 100 )
+	{
+		send_to_char( "Your partner is too tired from sparring to spar further.\n\r",ch );
+		return;
+	}
+    if( !IS_IMMORTAL( ch ) && !IS_NPC( ch ) && !IS_NPC( victim ) )
+    {
+    	if( !xIS_SET( ch->act, PLR_QUESTING ) && xIS_SET( victim->act, PLR_QUESTING ) )
+    	{
+    		send_to_char( "You can't spar a player involved in a role playing event.\n\r", ch );
+    		return;
+    	}
+    	if( xIS_SET( ch->act, PLR_QUESTING ) && !xIS_SET( victim->act, PLR_QUESTING ) )
+    	{
+    		send_to_char( "You can't spar a player not involved in a role playing event.\n\r", ch );
+    		return;
+    	}
+    }
+	if( !IS_NPC( ch ) && !IS_NPC( victim ) && victim->hit < 86 )
+    {
+        send_to_char( "They need to rest a bit before sparring some more.\n\r", ch );
+        return;
+    }
+	if( !IS_NPC( ch ) && !IS_NPC( victim ) && ch->hit < 86 )
+    {
+        send_to_char( "You need to rest a bit before sparring some more.\n\r", ch );
+        return;
+    }
+	if( !xIS_SET( ch->act, PLR_SPAR ) )
+		xSET_BIT( ch->act, PLR_SPAR );
+	
+    if( !xIS_SET( victim->act, PLR_SPAR ) )
+		xSET_BIT( victim->act, PLR_SPAR );
+    
+    if( is_safe( ch, victim, TRUE ) )
+	return;
+
+    if( ch->position == POS_RESTING || ch->position == POS_SLEEPING )
+    {
+      send_to_char( "How do you propose to do that in your current state?\n\r", ch );
+      return;
+    }
+    if( ch->position == POS_FIGHTING || ch->position ==  POS_EVASIVE || ch->position ==  POS_DEFENSIVE || ch->position ==  POS_AGGRESSIVE || ch->position ==  POS_BERSERK )
+    {
+        send_to_char( "You do the best you can!\n\r", ch );
+        return;
+    }
+
+	if( !xIS_SET( ch->in_room->room_flags, ROOM_ARENA ) )
+	{
+       	send_to_char( "You must be in an arena to spar someone.\n\r", ch );
+        return;
+    }
+	if(!IS_NPC( ch ) && !IS_NPC( victim ) && ch->exp <= 5000 )
+    {
+        send_to_char( "You can not fight other players while you are in training.\n\r", ch );
+        return;
+    }
+	if( !IS_NPC( ch ) && !IS_NPC( victim ) && victim->exp <= 5000 )
+    {
+        send_to_char( "You can not fight other players while they are in training.\n\r", ch );
+        return;
+    }
+	if( who_fighting( victim ) != NULL )
+    {
+        send_to_char( "It would not be honorable to interfere with some one else's battle.\n\r", ch );
+        return;
+    }
+	if( !IS_NPC( ch ) && !IS_NPC( victim ) && victim->hit < 2 )
+    {
+        send_to_char( "They are too hurt to fight anymore.\n\r", ch );
+        return;
+    }
+	if( !IS_NPC( ch ) && !IS_NPC( victim ) && ch->hit < 2 )
+    {
+        send_to_char( "You are too hurt to fight anymore.\n\r", ch );
+        return;
+    }
+	if( !IS_NPC( ch ) && !IS_NPC( victim ) && !IS_HC( ch ) && !IS_HC( victim ) && xIS_SET( ch->act, PLR_SPAR ) && xIS_SET( victim->act, PLR_SPAR ) && ( ( IS_GOOD( ch ) && !IS_GOOD( victim ) )
+			|| ( IS_EVIL( ch ) && !IS_EVIL( victim ) ) || ( IS_NEUTRAL( ch ) && !IS_NEUTRAL( victim ) )	) )
+    {
+        send_to_char( "You would not spar someone who is aligned that way.\n\r", ch );
+        return;
+    }
+
+    if( !victim->pcdata->HBTCPartner )
+    {
+        send_to_char( "They are not accepting sparring partners at this time.\n\r", ch );
+        return;
+    }
+
+    if( strcasecmp( ch->name, victim->pcdata->HBTCPartner ) )
+    {
+        send_to_char( "They do not want to spar with you.\n\r", ch );
+        return;
+    }
+/*
+		sprintf( log_buf, "PLAYER COMBAT: %s[%s] vs. %s[%s].",
+			ch->name, !xIS_SET(ch->act, PLR_SPAR) ? "DEADLY" : "SPARING",
+			victim->name, !xIS_SET(victim->act, PLR_SPAR) ? "DEADLY" : "SPARING");
+		log_string_plus( log_buf, LOG_NORMAL, ch->level );
+*/
+    WAIT_STATE( ch, 1 * PULSE_VIOLENCE );
+    if( !xIS_SET( ch->act, PLR_SPAR ) )
+    {
+        if( !is_leet( ch ) )
+            sprintf( buf, "Help!  I am being attacked by %s!", IS_NPC( ch ) ? ch->short_descr : ch->name );
+        else
+            sprintf( buf, "Omigawd!  %s atakin me!! STAWP!!11shift-one1", IS_NPC( ch ) ? ch->short_descr : ch->name );
+        if( IS_PKILL( victim ) )
+        {
+            do_wartalk( victim, buf );
+        }
+        else
+        {
+            do_yell( victim, buf );
+        }
+    }
+    check_illegal_pk( ch, victim );
+    ch->spar_start = ch->exp;
+    victim->spar_start = victim->exp;
+    ch->delay = 0;
+    check_attacker( ch, victim );
+    multi_hit( ch, victim, TYPE_UNDEFINED );
+    return;
+}
+
 void do_kill( CHAR_DATA* ch, const char* argument)
 {
    char arg[MAX_INPUT_LENGTH];
